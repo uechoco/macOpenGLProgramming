624b7a0cd02e596e700f73efe88e6c4e9c946975
ueno-yusuke <ueno.yusuke@dena.jp>
2018-07-15 12:32:51 +0900

    macOSでOpenGLプログラミング（1-2. ウィンドウのリサイズ方法の調整） https://qiita.com/sazameki/items/86d6f320cebd80ba2f77

---
+++ MyGLGame/WindowController.h
@@ -0,0 +1,13 @@
+//
+//  WindowController.h
+//  MyGLGame
+//
+//  Created by Yusuke Ueno on 2018/07/15.
+//  Copyright © 2018年 uechoco. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface WindowController : NSWindowController<NSWindowDelegate>
+
+@end
---
+++ MyGLGame/WindowController.m
@@ -0,0 +1,39 @@
+//
+//  WindowController.m
+//  MyGLGame
+//
+//  Created by Yusuke Ueno on 2018/07/15.
+//  Copyright © 2018年 uechoco. All rights reserved.
+//
+
+#import "WindowController.h"
+
+@interface WindowController ()
+
+@end
+
+@implementation WindowController
+
+- (void)windowDidLoad {
+    [super windowDidLoad];
+    
+    self.window.delegate = self;
+    self.window.frameAutosaveName = @"Main Window";
+    [self.window setContentSize:NSMakeSize(640, 480)];
+}
+
+- (NSSize)windowWillResize:(NSWindow *)sender toSize:(NSSize)frameSize
+{
+    if ([self.window styleMask] & NSWindowStyleMaskFullScreen)
+    {
+        return frameSize;
+    }
+    else
+    {
+        NSRect rect = NSMakeRect(0, 0, 640, 480);
+        rect = [self.window frameRectForContentRect:rect];
+        return rect.size;
+    }
+}
+
+@end
